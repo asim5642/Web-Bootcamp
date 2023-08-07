9192e02e62fb560ed5b9ecd20b7abd51efbcd352
Asim <136443891+asim5642@users.noreply.github.com>
2023-08-07 12:37:44 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// alert('Hi Asim');
+// // data types
+// // string ,number ,boolean
+//  a = prompt("Enter your Name");
+//  alert(a);
+//  document.write(a)
+ stdName =prompt("Enter your name");
+ std_TmMarks = +prompt("Enter your tm value");
+ std_obtMarks= +prompt("Enter your om value");
+ res=(std_obtMarks /std_TmMarks) * 100 ;
+ document.write( "Dear "+ stdName + " You have got it " + res + " %")
